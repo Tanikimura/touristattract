33aae38bece36b3023bd4155de8789509fc6a5a5
Gourville <cedric.gourville@gmail.com>
2017-04-23 08:24:52 +0200

    carousel done

--- js/custom.js
+++ js/custom.js
@@ -34,3 +34,11 @@ $(window).scroll(function() {
     else
         $('#returnOnTop').fadeIn();
 });
+
+$('.carousel-control.left').click(function() {
+  $('#myCarousel').carousel('prev');
+});
+
+$('.carousel-control.right').click(function() {
+  $('#myCarousel').carousel('next');
+});
